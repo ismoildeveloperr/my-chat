8e37d1dc3028c430dbda503ce4f6ac7b4af73802
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-23 23:38:55 +0500

    navlink

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <ul>
-                <li><a href="/profile"><h3>Профил</h3></a></li>
-                <li><a href="/messages"><h3>Сообщения</h3></a></li>
-                <li><a href="/news"><h3>Новости</h3></a></li>
-                <li><a href="/music"><h3>Музыка</h3></a></li>
-                <li><a href="/settings"><h3>Настройка</h3></a></li>
+                <li><NavLink to="/profile"><h3>Профил</h3></NavLink></li>
+                <li><NavLink to="/messages"><h3>Сообщения</h3></NavLink></li>
+                <li><NavLink to="/news"><h3>Новости</h3></NavLink></li>
+                <li><NavLink to="/music"><h3>Музыка</h3></NavLink></li>
+                <li><NavLink to="/settings"><h3>Настройка</h3></NavLink></li>
             </ul>
         </nav>
     );
